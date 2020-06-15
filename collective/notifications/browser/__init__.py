8a1f7eac5fa88e0544a2471d8229ddd26b599d9f
Joshua LaPlace <joshua.laplace@enfoldsystems.com>
2020-06-15 15:19:27 -0500

    Deprecate repo.

--- collective/notifications/browser/__init__.py
+++
@@ -1,6 +0,0 @@
-from zope.i18nmessageid import MessageFactory
-MessageFactory = MessageFactory('collective.notifications')
-
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
